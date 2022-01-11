e1e0bafad4f14b36707c0476f1a4c5deca821f78
Rida F'kih <hello@rida.dev>
2022-01-10 23:39:29 -0700

    declare Config as type

---
+++ src/types/config.d.ts
@@ -0,0 +1,10 @@
+declare interface Config {
+  unpackArrays?: boolean;
+  removeArrays?: boolean;
+  replaceProxyFunctions?: boolean;
+  removeProxyFunctions?: boolean;
+  simplifyExpressions?: boolean;
+  beautify?: boolean;
+  simplifyProperties?: boolean;
+  renameHexIdentifiers?: boolean;
+}
